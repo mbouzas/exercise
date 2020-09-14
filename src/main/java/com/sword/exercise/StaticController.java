d3e4bc46cea9b850a684022035f0d601eb6db3c4
Manuel Bouzas Reguera <manuel.bouzas@toposware.com>
2020-09-14 13:16:26 +0200

    Signed commit 2

--- src/main/java/com/sword/exercise/StaticController.java
+++ src/main/java/com/sword/exercise/StaticController.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+//test
 @Controller
 public class StaticController {
-
 
 	@RequestMapping(method = RequestMethod.GET, value = "/")
 	public String index() {
 	    return "index.html";
 	}
+
 }
